982f5ed4977a42c0372f44dccba29e21d3b3510c
Rossi Kwak <rossi.kwak@gmail.com>
2021-12-07 14:52:25 +0900

    [PC gStreamer] 데이터 전송값 표시를 Gbps로도 나타내기
    ref. http://tars.iptime.org:8989/redmine/issues/294

--- application/gStreamer/gStreamerDlg.cpp
+++ application/gStreamer/gStreamerDlg.cpp
@@ -601,7 +601,7 @@ void CgStreamerDlg::OnTimer(UINT_PTR nIDEvent)
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
 	if (nIDEvent == COUNT_REFRESH_TIMER) {
 		m_transferRate.SetPos((short)(m_curKBps/1000));	//KBps에서 MBps단위로 변환
-		m_KBps.Format(_T("%.0f KBps"), m_curKBps);
+		m_KBps.Format(_T("%.0fKBps (%.1fGbps)"), m_curKBps, 8.*m_curKBps/1000000.);
 		UpdateData(FALSE);
 	}
 
